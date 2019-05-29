e79ca40b57371be77e9f7a5768150c4c766bd41c
Justin O'Hara <justinrobertohara@gmail.com>
2019-05-28 20:51:02 -0700

    Component Properties aka "props"

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 // TODO
+var GroceryList = props => (
+  <ul>
+    <li>{props.grocery[0]}</li>
+    <li>{props.grocery[1]}</li>
+  </ul>
+);
+
+var App = () => (
+  <div>
+    <h2>My Grocery List</h2>
+    <GroceryList grocery={['Apple', 'Bananas']} />
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('app'));
