2f7c6e5897ac33f74f649f00baf3b0c4873e95ee
Justin O'Hara <justinrobertohara@gmail.com>
2019-05-29 19:17:33 -0700

    Bare Minimum Requirements

--- app.js
+++ app.js
@@ -7,15 +7,42 @@ var App = () => (
   </div>
 );
 
-var GroceryList = props => {
-  var groceries = props.grocery;
+var GroceryList = props => (
+  <ul>
+    {props.grocery.map(item => (
+      <GroceryListItem key={item} value={item} />
+    ))}
+  </ul>
+);
 
-  var groceryItems = groceries.map(item => (
-    <GroceryListItem key={item} value={item} />
-  ));
-  return <ul>{groceryItems}</ul>;
-};
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bold: false
+    };
+  }
 
-var GroceryListItem = props => <li>{props.value}</li>;
+  onHoverBold() {
+    this.setState({
+      bold: !this.state.bold
+    });
+  }
+
+  render() {
+    var style = {
+      fontWeight: this.state.bold ? 'bold' : 'normal'
+    };
+    return (
+      <li
+        style={style}
+        onMouseEnter={this.onHoverBold.bind(this)}
+        onMouseLeave={this.onHoverBold.bind(this)}
+      >
+        {this.props.value}
+      </li>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('app'));
