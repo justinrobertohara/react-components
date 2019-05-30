276c01971703c17890728977e7836b786fa25034
Justin O'Hara <justinrobertohara@gmail.com>
2019-05-29 18:55:53 -0700

    component properties aka props

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 // TODO
-var GroceryList = props => (
-  <ul>
-    <li>{props.grocery[0]}</li>
-    <li>{props.grocery[1]}</li>
-  </ul>
-);
 
 var App = () => (
   <div>
     <h2>My Grocery List</h2>
-    <GroceryList grocery={['Apple', 'Bananas']} />
+    <GroceryList grocery={['Apple', 'Bananas', 'Oranges', 'Kiwi']} />
   </div>
 );
+
+var GroceryList = props => {
+  var groceries = props.grocery;
+
+  var groceryItems = groceries.map(item => (
+    <GroceryListItem key={item} value={item} />
+  ));
+  return <ul>{groceryItems}</ul>;
+};
+
+var GroceryListItem = props => <li>{props.value}</li>;
 
 ReactDOM.render(<App />, document.getElementById('app'));
